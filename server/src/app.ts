697178088efe575be4c3760078f89bb7f3307961
ajackster <jacksonal300@gmail.com>
2018-07-07 13:56:27 -0400

    Set up Supplier api requests

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,6 +1,7 @@
-import express from 'express';
-import cors from 'cors';
-import graphqlHTTP from 'express-graphql';
+import * as express from 'express';
+import * as cors from 'cors';
+import * as graphqlHTTP from 'express-graphql';
+import * as bodyParser from 'body-parser';
 import schema from './graphql/schema';
 
 import { connectToMongo } from './lib/DBRef';
@@ -8,6 +9,7 @@ connectToMongo();
 
 const graphqlPort = 3000;
 const appPort = 8080;
+const serverIP = '108.18.137.11';
 
 const graphql = express();
 graphql.use(cors());
@@ -23,6 +25,7 @@ const app = express();
 app.use(cors());
 // Serve static files from the React app
 app.use(express.static(__dirname + '/../client/dist/'));
+app.use(bodyParser);
 
 import { GetDiamonds, GetDiamond } from './api/Diamonds';
 app.get('/GetDiamonds', GetDiamonds);
@@ -32,6 +35,6 @@ import { GetSuppliers, GetSupplier } from './api/Supplier';
 app.get('/GetSuppliers', GetSuppliers);
 app.post('/GetSupplier', GetSupplier);
 
-app.listen(appPort);
-console.log('App server running at localhost:' + appPort);
+app.listen(appPort, serverIP);
+console.log(`App server running at ${serverIP}:${appPort}`);
 
